f993720039a424f7b007fc1b8aa0cbf6dc52d87c
Aldrin Tilos <tilosaldrin@gmail.com>
2022-06-09 23:45:31 +0800

    Finalize Contact Tracing

--- Contact Tracing/Form1.cs
+++ Contact Tracing/Form1.cs
@@ -6,23 +6,15 @@ namespace Contact_Tracing
         {
             InitializeComponent();
         }
-
         private void label8_Click(object sender, EventArgs e)
         {
-
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
-
         private void lbl_ctracing_Click(object sender, EventArgs e)
-        {
-            
-
+        {   
         }
-
         private void btn_submit_Click(object sender, EventArgs e)
         {
             StreamWriter detail = new StreamWriter(@"F:\Contract_Tracing\Contact_Tracing.txt", true);
@@ -41,10 +33,8 @@ namespace Contact_Tracing
             txtbox_date.Text = txtbox_time.Text = txtbox_gender.Text = txtbox_dbirth.Text = txtbox_cyear.Text = txtbox_pname.Text = 
             txtbox_gname.Text = txtbox_address.Text = txtbox_cnumber.Text = txtbox_temp.Text = "";
         }
-
         private void txtbox_dbirth_TextChanged(object sender, EventArgs e)
         {
-
         }
     }
 }

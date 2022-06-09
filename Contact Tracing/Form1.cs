3fe37ea096b4d9fa9f34bcc0dcb4037e7dcf44d5
Aldrin Tilos <tilosaldrin@gmail.com>
2022-06-09 23:26:01 +0800

    Rename Variable

--- Contact Tracing/Form1.cs
+++ Contact Tracing/Form1.cs
@@ -25,18 +25,18 @@ namespace Contact_Tracing
 
         private void btn_submit_Click(object sender, EventArgs e)
         {
-            StreamWriter file = new StreamWriter(@"F:\Contract_Tracing\Contact_Tracing.txt", true);
-            file.WriteLine("Date: " + txtbox_date.Text);
-            file.WriteLine("Time: " + txtbox_time.Text);
-            file.WriteLine("Gender: " + txtbox_gender.Text);
-            file.WriteLine("Date of Birth: " + txtbox_dbirth.Text);
-            file.WriteLine("Grade and Section: " + txtbox_gsection.Text);
-            file.WriteLine("Name of Pupil: " + txtbox_pname.Text);
-            file.WriteLine("Name of Parent/Guardian: " + txtbox_gname.Text);
-            file.WriteLine("Address: " + txtbox_address.Text);
-            file.WriteLine("Contact Number: " + txtbox_cnumber.Text);
-            file.WriteLine("Temperature: " + txtbox_temp.Text);
-            file.Close();
+            StreamWriter detail = new StreamWriter(@"F:\Contract_Tracing\Contact_Tracing.txt", true);
+            detail.WriteLine("Date: " + txtbox_date.Text);
+            detail.WriteLine("Time: " + txtbox_time.Text);
+            detail.WriteLine("Gender: " + txtbox_gender.Text);
+            detail.WriteLine("Date of Birth: " + txtbox_dbirth.Text);
+            detail.WriteLine("Grade and Section: " + txtbox_gsection.Text);
+            detail.WriteLine("Name of Pupil: " + txtbox_pname.Text);
+            detail.WriteLine("Name of Parent/Guardian: " + txtbox_gname.Text);
+            detail.WriteLine("Address: " + txtbox_address.Text);
+            detail.WriteLine("Contact Number: " + txtbox_cnumber.Text);
+            detail.WriteLine("Temperature: " + txtbox_temp.Text);
+            detail.Close();
             MessageBox.Show("Thank You For Your Response!", "Contact Tracing", MessageBoxButtons.OK, MessageBoxIcon.Information);
             
         }

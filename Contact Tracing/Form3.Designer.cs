09b0e1f7a50e671bedb6cd624166082d75ac47f1
Aldrin Tilos <tilosaldrin@gmail.com>
2022-06-28 19:16:33 +0800

    Label, Button, TextBox, and DateTimePicker Added in Form3

--- Contact Tracing/Form3.Designer.cs
+++ Contact Tracing/Form3.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.lbl_date = new System.Windows.Forms.Label();
+            this.dtp_date = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.lbl_search = new System.Windows.Forms.Label();
+            this.btn_date = new System.Windows.Forms.Button();
+            this.btn_all = new System.Windows.Forms.Button();
+            this.lbl_all = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Font = new System.Drawing.Font("Swis721 Blk BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_date.Location = new System.Drawing.Point(131, 227);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(179, 22);
+            this.lbl_date.TabIndex = 0;
+            this.lbl_date.Text = "Search For Date";
+            // 
+            // dtp_date
+            // 
+            this.dtp_date.Location = new System.Drawing.Point(84, 261);
+            this.dtp_date.Name = "dtp_date";
+            this.dtp_date.Size = new System.Drawing.Size(262, 23);
+            this.dtp_date.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(701, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btn_search
+            // 
+            this.btn_search.BackColor = System.Drawing.Color.Transparent;
+            this.btn_search.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_search.BackgroundImage")));
+            this.btn_search.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_search.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn_search.Location = new System.Drawing.Point(706, 129);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(38, 23);
+            this.btn_search.TabIndex = 6;
+            this.btn_search.UseVisualStyleBackColor = false;
+            this.btn_search.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbl_search
+            // 
+            this.lbl_search.AutoSize = true;
+            this.lbl_search.Font = new System.Drawing.Font("Swis721 Blk BT", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_search.Location = new System.Drawing.Point(283, 83);
+            this.lbl_search.Name = "lbl_search";
+            this.lbl_search.Size = new System.Drawing.Size(227, 43);
+            this.lbl_search.TabIndex = 7;
+            this.lbl_search.Text = "Search Bar";
+            // 
+            // btn_date
+            // 
+            this.btn_date.Font = new System.Drawing.Font("Swis721 Blk BT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_date.Location = new System.Drawing.Point(147, 304);
+            this.btn_date.Name = "btn_date";
+            this.btn_date.Size = new System.Drawing.Size(135, 38);
+            this.btn_date.TabIndex = 8;
+            this.btn_date.Text = "Enter";
+            this.btn_date.UseVisualStyleBackColor = true;
+            // 
+            // btn_all
+            // 
+            this.btn_all.Font = new System.Drawing.Font("Swis721 Blk BT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_all.Location = new System.Drawing.Point(489, 304);
+            this.btn_all.Name = "btn_all";
+            this.btn_all.Size = new System.Drawing.Size(135, 38);
+            this.btn_all.TabIndex = 9;
+            this.btn_all.Text = "Enter";
+            this.btn_all.UseVisualStyleBackColor = true;
+            // 
+            // lbl_all
+            // 
+            this.lbl_all.AutoSize = true;
+            this.lbl_all.Font = new System.Drawing.Font("Swis721 Blk BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_all.Location = new System.Drawing.Point(464, 261);
+            this.lbl_all.Name = "lbl_all";
+            this.lbl_all.Size = new System.Drawing.Size(160, 22);
+            this.lbl_all.TabIndex = 10;
+            this.lbl_all.Text = "Search All List";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_all);
+            this.Controls.Add(this.btn_all);
+            this.Controls.Add(this.btn_date);
+            this.Controls.Add(this.lbl_search);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dtp_date);
+            this.Controls.Add(this.lbl_date);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lbl_date;
+        private DateTimePicker dtp_date;
+        private TextBox textBox1;
+        private Button btn_search;
+        private Label lbl_search;
+        private Button btn_date;
+        private Button btn_all;
+        private Label lbl_all;
     }
 }
--- Contact Tracing/Form3.cs
+++ Contact Tracing/Form3.cs
@@ -16,5 +16,10 @@ namespace Contact_Tracing
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

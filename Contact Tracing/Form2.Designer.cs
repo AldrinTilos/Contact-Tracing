2f4c3396c6e0235b709fc823a306ab0e78b331ad
Aldrin Tilos <tilosaldrin@gmail.com>
2022-06-28 18:25:52 +0800

    Label, TextBox, and Button Added in Form2

--- Contact Tracing/Form2.Designer.cs
+++ Contact Tracing/Form2.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.txtbox_user = new System.Windows.Forms.TextBox();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.txtbox_pass = new System.Windows.Forms.TextBox();
+            this.btn_admin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Location = new System.Drawing.Point(224, 210);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(33, 15);
+            this.lbl_user.TabIndex = 0;
+            this.lbl_user.Text = "User:";
+            // 
+            // txtbox_user
+            // 
+            this.txtbox_user.Location = new System.Drawing.Point(268, 210);
+            this.txtbox_user.Name = "txtbox_user";
+            this.txtbox_user.Size = new System.Drawing.Size(245, 23);
+            this.txtbox_user.TabIndex = 1;
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Location = new System.Drawing.Point(197, 247);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(60, 15);
+            this.lbl_pass.TabIndex = 3;
+            this.lbl_pass.Text = "Password:";
+            // 
+            // txtbox_pass
+            // 
+            this.txtbox_pass.Location = new System.Drawing.Point(268, 247);
+            this.txtbox_pass.Name = "txtbox_pass";
+            this.txtbox_pass.Size = new System.Drawing.Size(245, 23);
+            this.txtbox_pass.TabIndex = 2;
+            // 
+            // btn_admin
+            // 
+            this.btn_admin.Location = new System.Drawing.Point(330, 276);
+            this.btn_admin.Name = "btn_admin";
+            this.btn_admin.Size = new System.Drawing.Size(132, 41);
+            this.btn_admin.TabIndex = 4;
+            this.btn_admin.Text = "Enter";
+            this.btn_admin.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_admin);
+            this.Controls.Add(this.txtbox_pass);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.txtbox_user);
+            this.Controls.Add(this.lbl_user);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lbl_user;
+        private TextBox txtbox_user;
+        private Label lbl_pass;
+        private TextBox txtbox_pass;
+        private Button btn_admin;
     }
 }
